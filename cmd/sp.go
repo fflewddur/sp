339a5bd40d9dd0b96ca189811b53ea2506cc1668
Todd Kulesza <todd@dropline.net>
2019-08-12 10:05:44 -0700

    add xml parsing to the cli

--- cmd/sp.go
+++ cmd/sp.go
@@ -5,30 +5,42 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/fflewddur/sp/libsp"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "sp [file to parse]",
+	Use:   "sp [qsf file]",
 	Short: "sp is a survey parser for Qualtrics data",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		path := args[0]
+		qsfPath := args[0]
 
-		log.Printf("Reading '%s'", path)
-		qsf, err := os.Open(path) // For read access.
+		log.Printf("Reading '%s'", qsfPath)
+		qsf, err := os.Open(qsfPath)
 		if err != nil {
-			log.Fatalf("Error reading '%s': %s", path, err)
+			log.Fatalf("Error reading '%s': %s", qsfPath, err)
 		}
 		defer qsf.Close()
 
 		qsfReader := bufio.NewReader(qsf)
-
 		s, err := libsp.ReadQsf(qsfReader)
 		if err != nil {
-			log.Fatalf("Error parsing '%s': %s", path, err)
+			log.Fatalf("Error parsing '%s': %s", qsfPath, err)
+		}
+
+		xmlPath := buildXMLPath(qsfPath)
+		log.Printf("Reading '%s'", xmlPath)
+		xml, err := os.Open(xmlPath)
+		if err != nil {
+			log.Fatalf("Error reading '%s': %s", xmlPath, err)
+		}
+		xmlReader := bufio.NewReader(xml)
+		err = s.ReadXML(xmlReader)
+		if err != nil {
+			log.Fatalf("Error parsing '%s': %s", xmlPath, err)
 		}
 
 		log.Printf("Title = '%s', # of questions = %d, description = '%s'\n", s.Title, len(s.Questions), s.Description)
@@ -38,8 +50,14 @@ var rootCmd = &cobra.Command{
 	},
 	Version: "0.0.1",
 }
-var inputFile string
-var cfgFile string
+
+func buildXMLPath(qsfPath string) string {
+	i := strings.LastIndex(qsfPath, ".")
+	if i > 0 {
+		qsfPath = qsfPath[:i]
+	}
+	return qsfPath + ".xml"
+}
 
 // Execute runs the command
 func Execute() {
